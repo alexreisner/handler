535f16a1545996c012e65f82513bc2615d3536e3
Alex Reisner <alex@alexreisner.com>
2010-08-19 14:49:05 -0400

    Clean up whitespace.

--- lib/handler.rb
+++ lib/handler.rb
@@ -1,16 +1,16 @@
 module Handler
-	
+
   ##
   # Included hook: extend including class.
   #
   def self.included(base)
     base.extend ClassMethods
   end
-  
+
   ##
   # Transliterate a string: change accented Unicode characters to ASCII
   # approximations. Tries several methods, attempting the best first:
-  # 
+  #
   # 1. unidecode, if installed (http://rubyforge.org/projects/unidecode)
   # 2. iconv (included with Ruby, doesn't work with all Ruby versions)
   # 3. normalize, then remove un-normalizable characters
@@ -21,7 +21,7 @@ module Handler
       transliterate_with_normalization(string) or
       string
   end
-  
+
   ##
   # Transliterate a string with the unidecode library.
   # Return nil if unsuccessful (eg, library not available).
@@ -34,7 +34,7 @@ module Handler
       nil
     end
   end
-  
+
   ##
   # Transliterate a string with the iconv library.
   # Return nil if unsuccessful (eg, library not available).
@@ -46,7 +46,7 @@ module Handler
       nil
     end
   end
-  
+
   ##
   # Generate a handle from a string.
   #
@@ -60,9 +60,9 @@ module Handler
     str = str.delete('.\'"')     # no space
     str = str.gsub(/\W/, ' ')    # space
     str = str.gsub(/ +/, separator)
-    str   
+    str
   end
-  
+
   ##
   # Get the next handle in the sequence (for avoiding duplicates).
   #
@@ -73,7 +73,7 @@ module Handler
       handle + separator + "2"
     end
   end
-  
+
   ##
   # Transliterate a string using multibyte normalization,
   # then remove remaining non-ASCII characters. Taken from
@@ -82,13 +82,13 @@ module Handler
   def self.transliterate_with_normalization(string)
     string.mb_chars.normalize.gsub(/[^\x00-\x7F]+/, '').to_s
   end
-  
-  module ClassMethods
-  
+
+  module ClassMethod
+
     ##
     # Declare that a model generates a handle based on
     # a given attribute or method. Options include:
-    # 
+    #
     # <tt>:separator</tt> - character to place between words
     # <tt>:store</tt> - attribute in which to store handle
     # <tt>:unique</tt> - generate a handle which is unique among all records
@@ -98,13 +98,13 @@ module Handler
       options[:write_to]  ||= :handle
       options[:unique]      = true if options[:unique].nil?
 
-	    ##
-	    # Generate a URL-friendly name.
-	    #
+      ##
+      # Generate a URL-friendly name.
+      #
       define_method :generate_handle do
         h = Handler.generate_handle(send(attribute), options[:separator])
         if options[:unique]
-          
+
           # generate a condition for finding an existing record with a
           # given handle
           find_dupe = lambda{ |h|
@@ -124,7 +124,7 @@ module Handler
         end
         h
       end
-      
+
       ##
       # Assign the generated handle to the specified attribute.
       #
